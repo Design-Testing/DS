bb70a717cb136073429220ecdc874a4505966c5f
albolmtri <albolmtriclevercloud@gmail.com>
2019-07-29 18:14:35 +0200

    add repositository anotation to sponsorship repository

--- Item 1/Acme-Conference/src/main/java/repositories/SponsorshipRepository.java
+++ Item 1/Acme-Conference/src/main/java/repositories/SponsorshipRepository.java
@@ -5,9 +5,11 @@ import java.util.Collection;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import domain.Sponsorship;
 
+@Repository
 public interface SponsorshipRepository extends JpaRepository<Sponsorship, Integer> {
 
 	// =======================================
