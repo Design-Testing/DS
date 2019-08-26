4afd61bba4c13f31b4f77253c53734b6d0963901
Alba Olmo <albaolmotrinidad@gmail.com>
2019-08-26 22:10:05 +0200

    update submission service test

--- Item 1/Acme-Conference/src/test/java/services/SubmissionServiceTest.java
+++ Item 1/Acme-Conference/src/test/java/services/SubmissionServiceTest.java
@@ -1,6 +1,9 @@
 
 package services;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +12,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import utilities.AbstractTest;
+import domain.Paper;
 import domain.Submission;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -31,23 +35,35 @@ public class SubmissionServiceTest extends AbstractTest {
 	private PaperService		paperService;
 
 
+	/* ========================= Test Create and Submits =========================== */
+
 	@Test
 	public void driverCreateAndSubmits() {
 
+		final Collection<String> authors = new ArrayList<String>();
+		authors.add("author1");
+		final Paper paper = this.paperService.create();
+		paper.setDocument("http://www.document.com");
+		paper.setSummary("summary");
+		paper.setTitle("title");
+		paper.setAuthors(authors);
+
 		final Object testingData[][] = {
 			{
-				//				A: Educafy Crear y guardar una Reservation
-				//				B: Test Positivo: Creación correcta de una Reservation
-				//				C: % Recorre 196 de la 196 lineas posibles
-				//				D: % cobertura de datos=8/32 (casos cubiertos / combinaciones posibles de atributos entre ellos)
-				"author1", "conference1", "paper1", null
+				//				A: Acme-Conference: un autor puede solicitar su presentación en una conferencia
+				//				B: Test Positivo: autor solicita correctamente una presentación
+				"author1", "conference7", paper, null
+			}, {
+				//				A: Acme-Conference: un autor puede solicitar su presentación en una conferencia
+				//				B: Test Positivo: autor intenta solicitar presentación a una conferencia cuyo plazo de presentación ha expirado
+				"author1", "conference6", paper, java.lang.IllegalArgumentException.class
 			}
 
 		};
 		for (int i = 0; i < testingData.length; i++)
-			this.templateCreateAndSubmits((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2], (Class<?>) testingData[i][3]);
+			this.templateCreateAndSubmits((String) testingData[i][0], (String) testingData[i][1], (Paper) testingData[i][2], (Class<?>) testingData[i][3]);
 	}
-	private void templateCreateAndSubmits(final String author, final String conference, final String paper, final Class<?> expected) {
+	private void templateCreateAndSubmits(final String author, final String conference, final Paper paper, final Class<?> expected) {
 
 		Class<?> caught;
 		Submission submission;
@@ -57,7 +73,7 @@ public class SubmissionServiceTest extends AbstractTest {
 			submission = this.submissionService.create(this.getEntityId(conference));
 			submission.setAuthor(this.authorService.findOne(this.getEntityId(author)));
 			submission.setConference(this.conferenceService.findOne(this.getEntityId(conference)));
-			this.submissionService.submits(submission, this.paperService.findOne(this.getEntityId(paper)));
+			this.submissionService.submits(submission, paper);
 		} catch (final Throwable oops) {
 			caught = oops.getClass();
 		}
